a59849b68b932f9bb5cf34f1e5d9226869e2386e
unknown666 <20326530+klodi667@users.noreply.github.com>
2021-08-31 09:38:52 -0400

    Add files via upload

---
+++ hydra v2.bat
@@ -0,0 +1,176 @@
+@echo off
+
+
+color 0a
+cls
+
+echo        this software will be updated every 30 days
+echo        this software is open source.
+  
+echo      if you are a criminal organizations or just a small criminal send as an email
+echo                with your service we will shared with hydra protecting your privacy
+echo                  Secretsociety(underscore)hydra@protonmail.com
+
+
+pause
+echo [1] "****212 MILHÕES DADOS BRASIL****" 
+echo.
+echo [2] 12 Millions camera - Blockchain"
+echo.
+echo [3] 100,000 USA CC's"
+echo.
+echo [4] $$ HACKED BITCOIN WALLETS $$$ "
+echo.
+echo [5] $ Investments $"
+echo.
+echo [6] ACCESS DATABASE,HACK,SPY ON MESSAGE"
+echo.
+echo [7] ANABOLIC STEROIDS"
+echo.
+echo [8] ANONYMOUS Black Hat Hackers"
+echo.
+echo [9] ARE YOU LOOKING FOR HITMAN"
+echo.
+echo [10] Arrange a kidnapping"
+echo.
+echo [11] Barbuda Passport"
+echo.
+echo [12] BEST ANABOLIC STEROIDS FOR SALE"
+echo.
+echo [13] BEST DEALERS IN FIRE ARMS"
+echo.
+echo [14] Best Hackers on Deep web"
+echo.
+echo [15] BEST TRANSFERS"
+echo.
+echo [16] BITCARDS - PREPAID CARDS"
+echo.
+echo [17] bitcoin multiplying software"
+echo.
+echo [18] BLACK MARKET WEAPONS"
+echo.
+echo [19] BRING DOWN ANY ATM  BE RICH"
+echo.
+echo [20] Bullet Proof Vests ** us Import"
+echo.
+echo [21] Business opportunity Buy Bank Login"
+echo.
+echo [22] Buy 1  2 CC SSN DOB Track-1  2-FULLZ"
+echo.
+echo [23] Buy Carded Gadgets iPhone Laptops"
+echo.
+echo [24] Buy CryptoHJ v1.0"
+echo.
+echo [25] Buy directly from the US Military"
+echo.
+echo [26] Buy Drivers License, Passport"
+echo.
+echo [27] Buy Drivers License, Passport, ID Card, SSD, Fake Money, IELTS TOEFL"
+echo.
+echo [28] BUYER NEEDED FOR GOLD"
+echo.
+echo [29] Buy Guns Online"
+echo.
+echo.[30] BUY HIGH QUALITY BANK NOTES"
+echo.
+echo [31] Buy migration documents"
+echo.
+echo [32] BUY NEW USED FIRE ARMS"
+echo.
+echo [33] BUY ORIGINAL DOCUMENTS"
+echo.
+echo [34] Buy Original Documents Certificates"
+echo.
+echo [35] Buy Passport And More"
+echo.
+echo [36] Buy Passports, Counterfeit banknote"
+echo.
+echo [37] BUY POWDER POTASSIUM CYANIDE"
+echo.
+echo [38] BUY PURE GOLD AND DIAMOND"
+echo.
+echo [39] Buy quality counterfeit banknotes"
+echo.
+echo [40] Buy REGISTER,CERTIFICATES,DOCUMENTS"
+echo.
+echo [41] BUY REGISTERED  IELTS,TOELFS,GMAT,NEBOSH,ESOL WITHOUT EXAMS"
+echo.
+echo [42] BUY SSD Chemical solution "
+echo.
+echo [43] BUY TOP GRADE COUNTERFEIT MONEY "
+echo.
+echo [44] BUY UNDETECTABLE COUNTERFEIT MONEY"
+echo.
+echo [45] bypass criminal background checks, take back your freedom "
+echo.
+echo [46] Calling All Drug Cartels"
+echo.
+echo [47] Call to murder US politicians"
+echo.
+echo [48] CC DATABASE USA 30K"
+echo.
+echo [49] CELULAR ROUBADO E RECEITAS MÉDICAS"
+echo.
+echo [50]  CERTIFIED CANNABIS VENDOR ON DARKW"
+echo.
+echo [51] Change University grades"
+echo.
+echo [0] Leave 
+echo.
+echo.
+set /p menu= Choose:
+if /i %menu%== 1 start "" https://textuploader.com/taqtq
+if /i %menu%== 2 start "" https://textuploader.com/taqth
+if /i %menu%== 3 start "" https://textuploader.com/taqtg
+if /i %menu%== 4 start "" https://textuploader.com/taqtv
+if /i %menu%== 5 start "" https://textuploader.com/taqtw
+if /i %menu%== 6 start "" https://textuploader.com/taqty
+if /i %menu%== 7 start "" https://textuploader.com/taqti
+if /i %menu%== 8 start "" https://textuploader.com/taqte
+if /i %menu%== 9 start "" https://textuploader.com/taqr5
+if /i %menu%== 10 start ""https://textuploader.com/taqr1
+if /i %menu%== 11 start ""https://textuploader.com/taqrt
+if /i %menu%== 12 start ""https://textuploader.com/taqr9
+if /i %menu%== 13 start ""https://textuploader.com/taqr0
+if /i %menu%== 14 start ""https://textuploader.com/taqrk
+if /i %menu%== 15 start ""https://textuploader.com/taqbi
+if /i %menu%== 16 start ""https://textuploader.com/taq3s
+if /i %menu%== 17 start ""https://textuploader.com/taq35
+if /i %menu%== 18 start ""https://textuploader.com/taq3o
+if /i %menu%== 19 start ""https://textuploader.com/taq36
+if /i %menu%== 20 start ""https://textuploader.com/taq3j
+if /i %menu%== 21 start ""https://textuploader.com/taq34
+if /i %menu%== 22 start ""https://textuploader.com/taq3l
+if /i %menu%== 23 start ""https://textuploader.com/taq3q
+if /i %menu%== 24 start ""https://textuploader.com/taq3c
+if /i %menu%== 25 start ""https://textuploader.com/taq3x
+if /i %menu%== 26 start ""https://textuploader.com/taq3g
+if /i %menu%== 27 start ""https://textuploader.com/taq37
+if /i %menu%== 28 start ""https://textuploader.com/taq32
+if /i %menu%== 29 start ""https://textuploader.com/taq3v
+if /i %menu%== 30 start ""https://textuploader.com/taq3n
+if /i %menu%== 31 start ""https://textuploader.com/taq3y
+if /i %menu%== 32 start ""https://textuploader.com/taq33
+if /i %menu%== 33 start ""https://textuploader.com/taq3i
+if /i %menu%== 34 start ""https://textuploader.com/taq3e
+if /i %menu%== 35 start ""https://textuploader.com/taq38
+if /i %menu%== 36 start ""https://textuploader.com/taqis
+if /i %menu%== 37 start ""https://textuploader.com/taqi5
+if /i %menu%== 38 start ""https://textuploader.com/taqi1
+if /i %menu%== 39 start ""https://textuploader.com/taqit
+if /i %menu%== 40 start ""https://textuploader.com/taqir
+if /i %menu%== 41 start ""https://textuploader.com/taqi0
+if /i %menu%== 42 start ""https://textuploader.com/taqik
+if /i %menu%== 43 start ""https://textuploader.com/taqi6
+if /i %menu%== 44 start ""https://textuploader.com/taqi4
+if /i %menu%== 45 start ""https://textuploader.com/taqiq
+if /i %menu%== 46 start ""https://textuploader.com/taqic
+if /i %menu%== 47 start ""https://textuploader.com/taqih
+if /i %menu%== 48 start ""https://textuploader.com/taqig
+if /i %menu%== 49 start ""https://textuploader.com/taqiu
+if /i %menu%== 50 start ""https://textuploader.com/taqif
+if /i %menu%== 51 start ""https://textuploader.com/taqip
+
+if /i %menu%==0 exit
+
+
